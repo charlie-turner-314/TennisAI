d96724b0dc0b8d85efa72910e84b85565f3c2e69
Charlie Turner <turner30@lyra05.ib0.hpc.qut.edu.au>
2024-09-05 12:01:27 +1000

    fix dirs for lyra

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -110,7 +110,7 @@ parser.description = "Prepare tennis dataset for training."
 parser.add_argument(
     "--input_dir",
     type=str,
-    default="/home/charlie/Documents/Kyrgios_Medvedev_2022",
+    default="/home/turner30/Projects/Tennis/Clips",
     help="Directory with mp4 tennis clips from broadcast angle. Each video should be a single shot sequence.",
     dest="input_dir",
 )
@@ -118,7 +118,7 @@ parser.add_argument(
 parser.add_argument(
     "--output_dir",
     type=str,
-    default="/home/charlie/Documents/Kyrgios_Medvedev_2022/processed",
+    default="/home/turner30/Pojects/Tennis/Clips/processed",
     help="Directory to save processed pkl dump.",
     dest="output_dir",
 )
